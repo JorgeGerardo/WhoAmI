01c3c00dd4ab23653a66d3b867138880bb608b41
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-05-11 18:15:07 -0700

    Bassis settled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SummaryComponent } from './components/summary/summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SummaryComponent },
+    // { path: 'skills',  },
+    // { path: 'experience',  },
+    // { path: 'projects',  },
+    // { path: 'contact',  },
+];
